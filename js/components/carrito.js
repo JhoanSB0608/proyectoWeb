14eebdc278006bb3b619ebdb3c15a58faacc992f
JhoanSB0608 <156533638+JhoanSB0608@users.noreply.github.com.>
2024-05-20 11:24:23 -0500

    feat: :construction: The functionality to load products into the cart has been implemented, the project is still under construction

---
+++ js/components/carrito.js
@@ -0,0 +1,65 @@
+// Obtener productos del carrito desde el almacenamiento local
+let productosEnCarrito = JSON.parse(localStorage.getItem("productos-en-carrito"));
+
+// Seleccionar elementos del DOM
+const contenedorCarritoVacio = document.querySelector("#carrito-vacio");
+const contenedorCarritoProductos = document.querySelector("#carrito-productos");
+const contenedorCarritoAcciones = document.querySelector("#carrito-acciones");
+const contenedorCarritoComprado = document.querySelector("#carrito-comprado");
+let botonesEliminar = document.querySelectorAll(".carrito-producto-eliminar");
+const botonVaciar = document.querySelector("#carrito-acciones-vaciar");
+const contenedorTotal = document.querySelector("#total");
+const botonComprar = document.querySelector("#carrito-acciones-comprar");
+
+/**
+ * Cargar productos en el carrito.
+ */
+function cargarProductosCarrito() {
+    if (productosEnCarrito && productosEnCarrito.length > 0) {
+        // Mostrar contenedor de productos y acciones, ocultar carrito vacío
+        contenedorCarritoVacio.classList.add("disabled");
+        contenedorCarritoProductos.classList.remove("disabled");
+        contenedorCarritoAcciones.classList.remove("disabled");
+        contenedorCarritoComprado.classList.add("disabled");
+
+        // Limpiar contenedor de productos
+        contenedorCarritoProductos.innerHTML = "";
+
+        // Añadir cada producto al contenedor de productos
+        productosEnCarrito.forEach(producto => {
+            const div = document.createElement("div");
+            div.classList.add("carrito-producto");
+            div.innerHTML = `
+                <img class="carrito-producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+                <div class="carrito-producto-titulo">
+                    <small>Título</small>
+                    <h3>${producto.titulo}</h3>
+                </div>
+                <div class="carrito-producto-cantidad">
+                    <small>Cantidad</small>
+                    <p>${producto.cantidad}</p>
+                </div>
+                <div class="carrito-producto-precio">
+                    <small>Precio</small>
+                    <p>$${producto.precio}</p>
+                </div>
+                <div class="carrito-producto-subtotal">
+                    <small>Subtotal</small>
+                    <p>$${producto.precio * producto.cantidad}</p>
+                </div>
+                <button class="carrito-producto-eliminar" id="${producto.id}"><i class="bi bi-trash-fill"></i></button>
+            `;
+            contenedorCarritoProductos.append(div);
+        });
+
+        // Actualizar botones de eliminar y total del carrito
+        actualizarBotonesEliminar();
+        actualizarTotal();
+    } else {
+        // Mostrar carrito vacío y ocultar contenedor de productos y acciones
+        contenedorCarritoVacio.classList.remove("disabled");
+        contenedorCarritoProductos.classList.add("disabled");
+        contenedorCarritoAcciones.classList.add("disabled");
+        contenedorCarritoComprado.classList.add("disabled");
+    }
+}
